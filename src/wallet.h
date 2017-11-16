0680fe20e7eb222cbfeb047f1140cd2b3a172766
HashToBeWild <hashtobewild@cryptocurrency.ninja>
2017-11-16 02:21:20 +0200

    Mac Wallet Prep

--- src/wallet.h
+++ src/wallet.h
@@ -43,7 +43,7 @@ extern bool fSendFreeTransactions;
 extern bool fPayAtLeastCustomFee;
 
 //! -paytxfee default
-static const CAmount DEFAULT_TRANSACTION_FEE = 0.0001;
+static const CAmount DEFAULT_TRANSACTION_FEE = CAmount(0.0001);
 //! -paytxfee will warn if called with a higher fee than this amount (in satoshis) per KB
 static const CAmount nHighTransactionFeeWarning = 0.1 * COIN;
 //! -maxtxfee default

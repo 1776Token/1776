c97b668551ac669ab22281692beb826117c2711e
jk9694 <>
2018-09-20 19:24:11 -0400

    Commit to restart chain and fix copyright info

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -188,18 +188,11 @@ public:
         //vFixedSeeds.clear();
         //vSeeds.clear();
 
-		vSeeds.push_back(CDNSSeedData("1776-seed1.freedom.social", "1776-seed1.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("1776-seed2.freedom.social", "1776-seed2.freedom.social"));	
-		vSeeds.push_back(CDNSSeedData("1776-seed3.freedom.social", "1776-seed3.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("1776-seed4.freedom.social", "1776-seed4.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("1776-seed5.freedom.social", "1776-seed5.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("1776-seed6.freedom.social", "1776-seed6.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("1776-seed7.freedom.social", "1776-seed7.freedom.social"));
-		vSeeds.push_back(CDNSSeedData("45.63.87.169", "45.63.87.169"));
-		vSeeds.push_back(CDNSSeedData("149.28.207.135", "149.28.207.135"));
-		vSeeds.push_back(CDNSSeedData("95.179.141.12", "95.179.141.12"));
-		vSeeds.push_back(CDNSSeedData("95.179.143.241", "95.179.143.241"));
-		vSeeds.push_back(CDNSSeedData("140.82.51.218", "140.82.51.218"));		
+	vSeeds.push_back(CDNSSeedData("216.196.217.106", "216.196.217.106"));
+	vSeeds.push_back(CDNSSeedData("216.196.217.107", "216.196.217.107"));	
+	vSeeds.push_back(CDNSSeedData("216.196.217.108", "216.196.217.108"));
+	vSeeds.push_back(CDNSSeedData("216.196.217.109", "216.196.217.109"));
+	vSeeds.push_back(CDNSSeedData("216.196.217.110", "216.196.217.110"));		
 		
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 0x17);
--- src/init.cpp
+++ src/init.cpp
@@ -1,7 +1,9 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
-// Copyright (c) 2015-2017 The 1776 developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2017-2018 The COLX developers
+// Copyright (c) 2017-2018 The 1776 developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -508,7 +510,11 @@ std::string LicenseInfo()
            "\n" +
            FormatParagraph(strprintf(_("Copyright (C) 2014-%i The Dash Core Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
-           FormatParagraph(strprintf(_("Copyright (C) 2015-%i The 1776 Developers"), COPYRIGHT_YEAR)) + "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2015-%i The PIVX Developers"), COPYRIGHT_YEAR)) + "\n" +
+           "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2017-%i The COLX Developers"), COPYRIGHT_YEAR)) + "\n" +
+           "\n" +
+           FormatParagraph(strprintf(_("Copyright (C) 2017-%i The 1776 Developers"), COPYRIGHT_YEAR)) + "\n" +
            "\n" +
            FormatParagraph(_("This is experimental software.")) + "\n" +
            "\n" +
